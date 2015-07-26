1285644ebf35dfe0d3d058662e765fe43e28ef41
Guanqing <haoguanqing@live.com>
2015-07-26 05:03:40 -0400

    Algorithms: Design and Analysis
    Coursera/Stanford

---
+++ src/algorithms/CountInversions.java
@@ -0,0 +1,146 @@
+package algorithms;
+
+import java.awt.List;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CountInversions {
+	
+	public static void main(String[] args) {
+		long count = 0;
+		ArrayList<Integer> lst = new ArrayList();
+		try{
+			lst = new CountInversions().ReadFromFile("IntegerArray.txt");
+		}catch(FileNotFoundException e){
+			System.out.println("File not found!!!!!");
+		}finally {
+			if(lst!=null){
+				lst = new CountInversions().mergeSort(lst);
+			}
+			System.out.println(lst);
+		}
+
+	}
+	
+
+	public ArrayList<Integer> mergeSort(ArrayList<Integer> alst){
+		int len = alst.size();
+		long count = 0;
+		if (len==0 || len==1){
+			return alst;
+		} else {
+			int[] mergeList = new int[len];
+			ArrayList<Integer> lst1 = new ArrayList();
+			lst1.addAll(alst.subList(0, len/2));
+			ArrayList<Integer> lst2 = new ArrayList();
+			lst2.addAll(alst.subList(len/2, len-1));
+			lst1 = new CountInversions().mergeSort(lst1);
+			lst2 = new CountInversions().mergeSort(lst2);
+			int i=0;
+			int j=0;
+			for(int k=0;k<len;k++){
+				if (i==lst1.size()){
+					mergeList[k] = lst2.get(j++);
+				}
+				else if (j==lst2.size()){
+					mergeList[k] = lst1.get(i++);
+				}
+				else if (lst1.get(i)<lst2.get(j)){
+					mergeList[k] = lst1.get(i++);
+				}
+				else{
+					mergeList[k] = lst2.get(j++);
+				}
+			}
+			ArrayList<Integer> result = new ArrayList();
+			for (int s:mergeList){
+				result.add(s);
+			}
+			return result;
+		}
+	}
+
+	public long mergeCount(ArrayList<Integer> alst){
+		int len = alst.size();
+		if (len==0){
+			return 0;
+		} 
+		else if(len==1){
+			return 0;
+		}
+		else if(len==2){
+			if (alst.get(0)>alst.get(1)){
+				return 1;
+			}else{
+				return 0;
+			}
+		}
+		else {
+			int[] mergeList = new int[len];
+			ArrayList<Integer> lst1 = new ArrayList();
+			lst1.addAll(alst.subList(0, len/2));
+			long count1 = this.mergeCount(lst1);
+			int len1 = lst1.size();
+
+			ArrayList<Integer> lst2 = new ArrayList();
+			lst2.addAll(alst.subList(len/2, len-1));
+			long count2 = this.mergeCount(lst2);
+			int len2 = lst2.size();
+
+			ArrayList<Integer> lst3 = this.mergeSort(lst1);
+			ArrayList<Integer> lst4 = this.mergeSort(lst2);
+			long count3 = 0;
+			int i=0;
+			int j=0;
+			for(int k=0;k<len;k++){
+				if (i==lst3.size()){
+					mergeList[k] = lst4.get(j++);
+				}
+				else if (j==lst4.size()){
+					mergeList[k] = lst3.get(i++);
+					count3 = count3 + len1-1-i;
+				}
+				else if (lst3.get(i)<lst4.get(j)){
+					mergeList[k] = lst3.get(i++);
+				}
+				else{
+					mergeList[k] = lst4.get(j++);
+					count3 = count3 + len1-1-i;
+				}
+			}
+
+			return count1+count2+count3;
+		}
+	}
+
+	private long CountInversionSimple(ArrayList<Integer> lst){
+		long count = 0;
+		for (int i=0;i<lst.size();i++){
+			for (int j=i+1;j<lst.size();j++){
+				if(lst.get(i)>lst.get(j)){
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	
+	
+	//---------read from file------------
+	public ArrayList<Integer> ReadFromFile(String fileName) throws FileNotFoundException{
+		ArrayList<Integer> lst = new ArrayList();
+		Scanner sc = new Scanner(new FileReader(fileName));
+		while(sc.hasNextInt()){
+			lst.add(sc.nextInt());
+		}
+		sc.close();
+		return lst;
+	}
+
+
+}
+
---
+++ src/algorithms/QuickSort.java
@@ -0,0 +1,164 @@
+package algorithms;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class QuickSort {
+	public static int comparisonNumber = 0;
+	
+	public static void main(String[] args) {
+		int[] lst = new int[10000];
+		int[] lst2 = new int[10000];
+		int[] lst3 = new int[10000];
+		try{
+			lst = ReadFromFile("QuickSort.txt");
+			lst2 = ReadFromFile("QuickSort.txt");
+			lst3 = ReadFromFile("QuickSort.txt");
+		}catch(FileNotFoundException e){
+			System.out.println("File not found!!!!!");
+		}finally{
+			quickSortInit(lst);
+			System.out.print("quickSortInit - ");
+			getComparison();
+			
+			quickSortFinal(lst2);
+			System.out.print("quickSortFinal - ");
+			getComparison();
+			
+			quickSortMedian(lst3);
+			System.out.print("quickSortMedian - ");
+			getComparison();
+		}
+		
+	}
+
+	
+	//----------Quick sort methods----------
+
+	//choose the first element as the pivot
+	public static void quickSortInit(int[] lst){
+		if (lst.length==0 || lst.length==1) return;
+
+		clearComparison();
+		partitionInit(lst, 0, lst.length-1);
+	}
+	public static void partitionInit(int[] lst, int l, int r){
+		if (l>=r) return;
+
+		int p = lst[l];
+		int i = l+1;
+		for (int j=l+1;j<=r;j++){
+			if (lst[j]<p){
+				swapElements(lst, j, i);
+				i++;
+			}
+			addComparison();
+		}
+		swapElements(lst, l, i-1);
+		partitionInit(lst, l, i-2);
+		partitionInit(lst, i, r);
+	}
+	
+	//choose the final element as the pivot
+	public static void quickSortFinal(int[] lst){
+		if (lst.length==0 || lst.length==1) return;
+
+		clearComparison();
+		partitionFinal(lst, 0, lst.length-1);
+	}
+	public static void partitionFinal(int[] lst, int l, int r){
+		if (l>=r) return;
+		swapElements(lst, l, r);
+		
+		int p = lst[l];
+		int i = l+1;
+		for (int j=l+1;j<=r;j++){
+			if (lst[j]<p){
+				swapElements(lst, j, i);
+				i++;
+			}
+			addComparison();
+		}
+		swapElements(lst, l, i-1);
+		partitionFinal(lst, l, i-2);
+		partitionFinal(lst, i, r);
+	}
+	
+	//choose the median among 3 as the pivot
+	public static void quickSortMedian(int[] lst){
+		if (lst.length==0 || lst.length==1) return;
+
+		clearComparison();
+		partitionMedian(lst, 0, lst.length-1);
+	}
+	public static void partitionMedian(int[] lst, int l, int r){
+		if (l>=r) return;
+		int midIndex = l+ (r-l+2)/2-1;
+		int median = getMedian(lst, l, r);
+		if(median==r){
+			swapElements(lst, l, r);
+		}else if(median==midIndex){
+			swapElements(lst, l, midIndex);
+		}
+		
+		
+		int p = lst[l];
+		int i = l+1;
+		for (int j=l+1;j<=r;j++){
+			if (lst[j]<p){
+				swapElements(lst, j, i);
+				i++;
+			}
+			addComparison();
+		}
+		swapElements(lst, l, i-1);
+		partitionMedian(lst, l, i-2);
+		partitionMedian(lst, i, r);
+	}
+
+	//helper functions
+	public static void swapElements(int[] lst, int l, int r){
+		int z = lst[l];
+		lst[l] = lst[r];
+		lst[r] = z;
+	}
+	public static void getComparison(){
+		System.out.println("Comparisons: "+comparisonNumber);
+	}
+	public static void addComparison(){
+		comparisonNumber++;
+	}
+	public static void clearComparison(){
+		comparisonNumber=0;
+	}
+	
+	public static int getMedian(int[] lst, int l, int r){
+		int m = l+ (r-l+2)/2-1;
+		
+		int a = lst[l];
+		int b = lst[m];
+		int c = lst[r];
+		
+		if ((a<b&&b<c)||(c<b&&b<a)){
+			return m;
+		}else if ((a<c&&c<b)||(b<c&&c<a)){
+			return r;
+		}else{
+			return l;
+		}
+	}
+	
+	//----------read from file----------
+	public static int[] ReadFromFile(String fileName) throws FileNotFoundException{
+		int[] lst = new int[10000];
+		Scanner sc = new Scanner(new FileReader(fileName));
+		int i=0;
+		while(sc.hasNextInt()){
+			lst[i] = sc.nextInt();
+			i++;
+		}
+		sc.close();
+		return lst;
+	}
+}

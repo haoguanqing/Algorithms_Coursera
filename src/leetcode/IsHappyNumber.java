401d6c065c15a7fdf07cf9041a8020e2b926bcff
Guanqing <haoguanqing@live.com>
2015-10-24 16:25:52 -0400

    addDigits

--- src/leetcode/IsHappyNumber.java
+++ src/leetcode/IsHappyNumber.java
@@ -20,6 +20,24 @@ public class IsHappyNumber {
         }
         return set.contains(1);
     }
+	
+	//Given a non-negative integer num, 
+	//repeatedly add all its digits until the result has only one digit.
+	public int addDigits(int num){
+		if(num<0){
+			return 0;
+		}
+		if (num<10){
+			return num;
+		}
+		int sum = 0;
+		String s = num + "";
+		String[] ss = s.split("");
+		for (String str:ss){
+			sum+= Integer.parseInt(str);
+		}
+		return addDigits(sum);
+	}
 
 	public static void main(String[] args) {
 		for (int i=0;i<10;i++){
